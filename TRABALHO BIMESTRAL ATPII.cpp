0f874fea4f24987d494d5c1e72eace093aa8fd92
LEONARDO PICOLO DOS SANTOS DOS RANUCI <leonardoranuci17@gmail.com>
2025-09-15 19:24:14 -0300

    Add files via upload

---
+++ TRABALHO BIMESTRAL ATPII.cpp
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+#include<ctype.h>
+#include<conio2.h>
+
+//FUNCOES A SEREM INICIALIZADA
+
+
+void Executar(){
+	
+	do{
+		
+		switch(opcao){
+			//CADASTRO
+			case 'A':
+				break;
+			//EXCLUSAO
+			case 'B':
+				break;
+			//ALTERAÇAO
+			case 'C':
+				break;
+			//LISTAGEM
+			case 'D':
+				break;
+			//LISTAGEM DE CLIENTES COM 3 OU MAIS LOCAÇOES
+			case 'E':
+				break;		
+			//LISTAGEM DE TODOS OS CARROS DA CATEGORIA ESCOLHIDAS PELO USUARIO
+			case 'F':
+				break;
+			//MOSTRAR LOCAÇOES DE VALOR MAIOR QUE O INFORMADO
+			case 'G':
+				break;
+			//RELATORIO COMPLETO
+			case 'H':
+				break;
+			//CADASTRO AUTOMATICO	
+			case 'I':
+				break;
+		}
+	}while(opcao!=27);
+}
+
+
+
+
+int main(void){
+	
+	Executar();
+	
+	return 0;
+}
